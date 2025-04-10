a3322833c3b744cc4822bc319bc0730b6552bf32
Victor Ikuomola <vheecthurikuomola@gmail.com>
2025-04-10 17:21:12 +0100

    feat: adds application config

---
+++ config/config.go
@@ -0,0 +1,48 @@
+package config
+
+import (
+	"fmt"
+	"github.com/caarlos0/env/v11"
+)
+
+type Env string
+
+const (
+	Env_Test Env = "test"
+	Env_Dev  Env = "dev"
+)
+
+type Config struct {
+	DatabaseName     string `env:"DB_NAME"`
+	DatabaseHost     string `env:"DB_HOST"`
+	DatabasePort     string `env:"DB_PORT"`
+	DatabasePortTest string `env:"DB_PORT_TEST"`
+	DatabaseUser     string `env:"DB_USER"`
+	DatabasePassword string `env:"DB_PASSWORD"`
+	Env              Env    `env:"ENV" envDefault:"dev"`
+	ProjectRoot      string `env:"PROJECT_ROOT"`
+}
+
+func (c *Config) DatabaseUrl() string {
+	port := c.DatabasePort
+
+	if c.Env == Env_Test {
+		port = c.DatabasePortTest
+	}
+
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		c.DatabaseUser,
+		c.DatabasePassword,
+		c.DatabaseHost,
+		port,
+		c.DatabaseName,
+	)
+}
+
+func New() (*Config, error) {
+	cfg, err := env.ParseAs[Config]()
+	if err != nil {
+		return nil, fmt.Errorf("failed to load config: %w", err)
+	}
+	return &cfg, nil
+}
