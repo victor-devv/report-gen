78fc425241fabc3e1b530eb7c6e16913a2a08ca5
Victor Ikuomola <vheecthurikuomola@gmail.com>
2025-04-10 18:55:45 +0100

    feat: adds create and fetch user functionality

---
+++ fixtures/env.go
@@ -0,0 +1,59 @@
+package fixtures
+
+import (
+	"asyncapi/config"
+	"asyncapi/store"
+	"database/sql"
+	"fmt"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/stretchr/testify/require"
+)
+
+type TestEnv struct {
+	Config *config.Config
+	Db     *sql.DB
+}
+
+func NewTestEnv(t *testing.T) *TestEnv {
+	os.Setenv("ENV", string(config.Env_Test))
+	conf, err := config.New()
+	//assert that no error occurred
+	require.NoError(t, err)
+
+	db, err := store.NewPostgresDb(conf)
+
+	return &TestEnv{
+		Config: conf,
+		Db:     db,
+	}
+}
+
+func (te *TestEnv) SetupDb(t *testing.T) func(t *testing.T) {
+	// create database tables
+	m, err := migrate.New(
+		fmt.Sprintf("file:///%s/migrations", te.Config.ProjectRoot),
+		te.Config.DatabaseUrl())
+	require.NoError(t, err)
+
+	// throw error if no db changes (the db shouldd be empty at the start of the test)
+	if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+		require.NoError(t, err)
+	}
+
+	return te.TeardownDb
+}
+
+func (te *TestEnv) TeardownDb(t *testing.T) {
+	_, err := te.Db.Exec(fmt.Sprintf("TRUNCATE TABLE %s", strings.Join([]string{
+		"users",
+		"reresh_tokens",
+		"reports",
+	}, ", ")))
+	require.NoError(t, err)
+}
---
+++ store/users.go
@@ -0,0 +1,91 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+	"encoding/base64"
+	"fmt"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type UserStore struct {
+	db *sqlx.DB
+}
+
+func NewUserStore(db *sql.DB) *UserStore {
+	return &UserStore{
+		db: sqlx.NewDb(db, "postgres"),
+	}
+}
+
+type User struct {
+	Id                   uuid.UUID `db:"id"`
+	Email                string    `db:"email"`
+	HashedPasswordBase64 string    `db:"hashed_password"`
+	CreatedAt            time.Time `db:"created_at"`
+}
+
+func (u *User) ComparePassword(password string) error {
+	hashedPassword, err := base64.StdEncoding.DecodeString(u.HashedPasswordBase64)
+	if err != nil {
+		return err
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		return fmt.Errorf("invalid password")
+	}
+
+	return nil
+}
+
+func (s *UserStore) CreateUser(ctx context.Context, email, password string) (*User, error) {
+	const dml = `INSERT INTO users (email, hashed_password) VALUES ($1, $2) RETURNING *`
+	var user User
+
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, fmt.Errorf("failed to hash password: %w", err)
+	}
+
+	hashedPasswordBase64 := base64.StdEncoding.EncodeToString(bytes)
+
+	if err := s.db.GetContext(ctx, &user, dml, email, hashedPasswordBase64); err != nil {
+		return nil, fmt.Errorf("failed to create user: %w", err)
+	}
+
+	return &user, nil
+}
+
+func (s *UserStore) ById(ctx context.Context, userId uuid.UUID) (*User, error) {
+	const query = `SELECT * FROM users WHERE id = $1`
+	var user User
+
+	if err := s.db.GetContext(ctx, &user, query, userId); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("user not found")
+		}
+		return nil, fmt.Errorf("failed to fetch user by id: %w", err)
+	}
+
+	return &user, nil
+}
+
+func (s *UserStore) ByEmail(ctx context.Context, email string) (*User, error) {
+	const query = `SELECT * FROM users WHERE email = $1`
+	var user User
+
+	if err := s.db.GetContext(ctx, &user, query, email); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, fmt.Errorf("user not found")
+		}
+		return nil, fmt.Errorf("failed to fetch user: %w", err)
+	}
+
+	return &user, nil
+}
